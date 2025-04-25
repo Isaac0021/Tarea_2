7faa3f353c067a10b4c0ecf638ec1d8e32fc4227
Isaac <castillovega.icv@gmail.com>
2025-04-25 13:35:12 -0600

    primer commit

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+users = []
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+
+@app.post("/users", status_code=201)
+def create_user(user: User):
+    users.append(user)
+    return {"message": "Usuario creado", "user": user}
